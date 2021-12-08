ca3b1a9886c1431d59d955d04a54e11fc8414872
DogRoss <edgaragunther@gmail.com>
2021-12-07 17:54:51 -0800

    (AIE->GIT) work on title screen and prettiness
    work on animations and execute through animations
    IMPLEMENT CREDITS

---
+++ DashHell/Assets/ScriptsNoOrganize/TitleCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    bool canRotate = false;
+    bool rotated = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        //before this happens, the initial animation needs to happen, execute this in script
+        if(canRotate == true)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(90, 0, 0), Time.deltaTime);
+            rotated = true;
+        }
+
+        if (rotated == true)
+        {
+            //start the portal sequence
+        }
+    }
+
+    public void Rotate()
+    {
+        canRotate = true;
+    }
+}
